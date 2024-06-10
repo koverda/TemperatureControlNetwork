38e59a88d673ad447bb8ebb9dbd27dfbbd8a1ab0
Peter Koverda <peter@pulsegrow.com>
2024-06-10 16:54:48 -0700

    message types

---
+++ Messages.cs
@@ -0,0 +1,25 @@
+﻿namespace TemperatureControlNetwork;
+
+public enum MessageType
+{
+    Data,
+    Control
+}
+
+public abstract class Message
+{
+    public abstract MessageType Type { get; }
+}
+
+public class DataMessage : Message
+{
+    public override MessageType Type => MessageType.Data;
+    public string Data { get; set; }
+}
+
+public class ControlMessage : Message
+{
+    public override MessageType Type => MessageType.Control;
+    public int WorkerId { get; set; }
+    public bool Activate { get; set; }
+}
