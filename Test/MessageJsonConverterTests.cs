b3d47693015487fc8b7909cef43bf6ba2f21b477
Peter Koverda <peter@pulsegrow.com>
2024-06-11 14:21:12 -0700

    msg conv test

---
+++ Test/MessageJsonConverterTests.cs
@@ -0,0 +1,61 @@
+using System.Text.Json;
+using FluentAssertions;
+using TemperatureControlNetwork;
+
+namespace Test;
+
+public class MessageJsonConverterTests
+{
+    private readonly JsonSerializerOptions _jsonOptions = new()
+    {
+        Converters = { new MessageJsonConverter() }
+    };
+
+    [Fact]
+    public void ShouldSerializeAndDeserializeDataMessage()
+    {
+        // Arrange
+        var originalMessage = new DataMessage("Test Data");
+
+        // Act
+        string json = JsonSerializer.Serialize(originalMessage, _jsonOptions);
+        var deserializedMessage = JsonSerializer.Deserialize<Message>(json, _jsonOptions);
+
+        // Assert
+        deserializedMessage.Should().BeOfType<DataMessage>();
+        deserializedMessage.As<DataMessage>().Data.Should().Be("Test Data");
+    }
+
+    [Fact]
+    public void ShouldSerializeAndDeserializeControlMessage()
+    {
+        // Arrange
+        var originalMessage = new ControlMessage
+        {
+            WorkerId = 1,
+            Activate = true
+        };
+
+        // Act
+        string json = JsonSerializer.Serialize(originalMessage, _jsonOptions);
+        var deserializedMessage = JsonSerializer.Deserialize<Message>(json, _jsonOptions);
+
+        // Assert
+        deserializedMessage.Should().BeOfType<ControlMessage>();
+        deserializedMessage.As<ControlMessage>().WorkerId.Should().Be(1);
+        deserializedMessage.As<ControlMessage>().Activate.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ShouldThrowExceptionForUnknownMessageType()
+    {
+        // Arrange
+        string json = "{\"Type\":\"Unknown\",\"Data\":\"Test Data\"}";
+
+        // Act
+        Action act = () => JsonSerializer.Deserialize<Message>(json, _jsonOptions);
+
+        // Assert
+        act.Should().Throw<JsonException>().WithMessage("Unknown message type: Unknown");
+    }
+}
