767dca7481ad702b063d76c0cbcce45ba8abad2e
Peter Koverda <peter@pulsegrow.com>
2024-06-11 15:46:32 -0700

    util class start

---
+++ TemperatureControlNetwork/Core/Utils.cs
@@ -0,0 +1,20 @@
+﻿namespace TemperatureControlNetwork.Core
+{
+    public static class Utils
+    {
+        private static readonly Random _random = new Random();
+
+        /// <summary>
+        /// Adjusts the temperature by a small random value within the specified range.
+        /// </summary>
+        /// <param name="temperature">The original temperature value.</param>
+        /// <param name="maxAdjustment">The maximum adjustment value.</param>
+        /// <returns>The adjusted temperature value.</returns>
+        public static double AdjustTemperature(double temperature, double maxAdjustment = .1)
+        {
+            // Generate a random adjustment value between -maxAdjustment and +maxAdjustment
+            double adjustment = (_random.NextDouble() * 2 - 1) * maxAdjustment;
+            return temperature + adjustment;
+        }
+    }
+}
